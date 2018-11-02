790793577daac7057a920d0417f6186a9741cdcb
Travis Bumgarner <travis-bumgarner@pluralsight.com>
2018-11-02 15:52:44 -0400

    Added problem

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+// Given a string of alphabetical characters, how would you compress it? For example, ‘aaabbbcc’ => ‘a3b3c2’. 
+// The output string’s length is always equal to or smaller than the input string
+
 const sum = (a,b) => {
     return a + b
 }
